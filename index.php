eaa181e995d5614927b039bed593b7a7b1bb22ad
jostv23 <jost.vedlin@student.um.si>
2022-04-09 23:14:32 +0200

    izdelal osnovni izgled nav-barja

--- index.php
+++ index.php
@@ -4,9 +4,20 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Spletna trgovina</title>
 </head>
 <body>
-    <p>Pozdravljen</p>
+    <!-- NAVIGATION BAR -->
+    <nav>
+        <div class="nav-logo"> <h4>SPLETNA TRGOVINA</h4> </div>
+        
+        <ul class="nav-a">
+            <li><a href="#">Domov</a></li>
+            <li><a href="#">O nas</a></li>
+            <li><a href="#">Kontakt</a></li>
+            <li><a href="#">Registracija</a></li>
+        </ul>
+    </nav>
 </body>
 </html>
