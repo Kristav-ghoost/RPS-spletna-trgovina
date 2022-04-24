1918bcff2ee61910201c9ee597b93573d5b1d11c
Albert Duhanaj <albertduhanaj@Alberts-MacBook-Air.local>
2022-04-25 01:57:07 +0200

    Dodan templating

---
+++ views/footer.php
@@ -0,0 +1,5 @@
+<footer class="page-footer font-small blue pt-4 fixed-bottom">
+    <div class="footer-copyright text-center py-3">© 2022 Copyright:
+        <a href="/"> Programerz</a>
+    </div>
+</footer>
---
+++ views/navbar.php
@@ -0,0 +1,24 @@
+<nav class="navbar navbar-dark navbar-expand bg-primary justify-content-between">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"><i class="fa-brands fa-opencart"></i> </span><span class="d-none d-sm-inline px-1">e-commerce</span></a>
+        <ul class="navbar-nav">
+            <li class="nav-item text-center">
+                <a href="index.php" class="nav-link" data-toggle="modal" data-target="#"><span class="fa fa-house"></span><span class="d-none d-sm-inline  px-1">Domov</span></a>
+            </li>
+            <li class="nav-item text-center">
+                <a href="#" class="nav-link" data-toggle="modal" data-target="#"><span class="fa fa-circle-info"></span><span class="d-none d-sm-inline px-1">O nas</span></a>
+            </li>
+            <li class="nav-item text-center">
+                <a href="#" class="nav-link" data-toggle="modal" data-target="#"><span class="fa fa-address-book"></span><span class="d-none d-sm-inline  px-1">Kontakt</span></a>
+            </li>
+        </ul>
+        <ul class="nav navbar-nav">
+            <li class="nav-item text-center" id="signup-btn">
+                <a href="register.php" class="nav-link" data-toggle="modal" data-target="#"><span class="fa fa-user"></span><span class="d-none d-sm-inline px-1">Registracija</span></a>
+            </li>
+            <li class="nav-item text-center" id="login-btn">
+                <a href="#" class="nav-link" data-toggle="modal" data-target="#"><span class="fa fa-sign-in"></span><span class="d-none d-sm-inline px-1"> Prijava </span></a>
+            </li>
+        </ul>
+    </div>
+</nav>
