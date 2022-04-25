afc52a2a534a1be98436032303d5666f428f838e
jostv23 <jost.vedlin@student.um.si>
2022-04-25 21:53:42 +0200

    Dodaj login, grid layout za prikaz izdelkov in prikaz posameznega izdelka

--- index.php
+++ index.php
@@ -5,6 +5,49 @@
 <body class="d-flex flex-column min-vh-100">
     <?php include 'views/navbar.php';?>
 
+    <!-- SEARCH BAR -->
+    <div style="margin: 25px 300px 0px 300px;">
+        <div class="input-group">
+            <input type="search" class="form-control rounded" placeholder="Search" aria-label="Search" aria-describedby="search-addon" />
+            <button type="button" class="btn btn-outline-primary">search</button>
+        </div>
+    </div> 
+
+
+    <!-- LAYOUT -->
+    <div style="margin: 100px;">
+        <div class="container">
+
+            <?php for($i = 1; $i <= 3; $i++){ ?>
+                <div class="row">
+                    <?php for($j = 1; $j <= 4; $j++){ ?>
+                        <div class="col-md-3 col-sm-6">
+                            <div class="product-grid2">
+                                <div class="product-image2">
+                                    <a href="#">
+                                        <img class="pic-1" src="https://wallpaperaccess.com/full/2665514.jpg">
+                                        <img class="pic-2" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4uPw5zlsCR94cwrDehcEeBufwIntzSFxGFg&usqp=CAU">
+                                    </a>
+                                    <ul class="social">
+                                        <li><a href="#" data-tip="Quick View"><i class="fa fa-eye"></i></a></li>
+                                        <li><a href="#" data-tip="Add to Cart"><i class="fa fa-shopping-cart"></i></a></li>
+                                    </ul>
+                                    <a class="add-to-cart" href="#">Add to cart</a>
+                                </div>
+                                <div class="product-content">
+                                    <h3 class="title"><a href="artikel.php">Beetlejuice</a></h3>
+                                    <span class="price">$420.69</span>
+                                </div>
+                            </div>
+                        </div>
+                    <?php } ?>    
+                </div>
+            <?php } ?>
+
+        </div>
+    </div>
+
+
     <?php include 'views/footer.php';?>
 </body>
 </html>
