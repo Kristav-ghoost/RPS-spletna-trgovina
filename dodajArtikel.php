f01bedd50fabf8460fdc23940aea37f0c9b65654
svensekrene <rene.svensek@student.um.si>
2022-04-28 11:54:06 +0200

    Dodal dodajanje artiklov in spremenil navbar

---
+++ dodajArtikel.php
@@ -0,0 +1,64 @@
+<?php 
+include 'views/head.php';
+include 'glava.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<body class="d-flex flex-column min-vh-100">
+    <?php include 'views/navbar.php';?>
+    <section class="vh-100">
+    <div class="container h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+        <div class="col-lg-12 col-xl-11">
+            <div class="card text-black" >
+            <div class="card-body">
+                <div class="row justify-content-center">
+                <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+
+                    <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">Dodaj artikel</p>
+
+                    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST" class="mx-1 mx-md-4">
+
+                    <div class="d-flex flex-row align-items-center mb-4">
+                        <i class="fa fa-text-width fa-lg me-3 fa-fw"></i>
+                        <div class="form-outline flex-fill mb-0">
+                            <label class="form-label" for="imeartikel">Ime artikla</label>
+                            <input type="text" id="imeartikel" name="imeartikel" class="form-control" />
+                        </div>
+                    </div>
+
+                    <div class="d-flex flex-row align-items-center mb-4">
+                        <i class="fas fa-envelope fa-lg me-3 fa-fw"></i>
+                        <div class="form-outline flex-fill mb-0">
+                            <label class="form-label" for="opis">Opis</label>
+                            <textarea id="opis" name="opis" rows="4" class="form-control"></textarea>
+                        </div>
+                    </div>
+
+                    <div class="d-flex flex-row align-items-center mb-4">
+                        <i class="fas fa-money-bill fa-lg me-3 fa-fw"></i>
+                        <div class="form-outline flex-fill mb-0">
+                            <label class="form-label" for="cena">Cena</label>
+                            <input type="number" id="cena" name="cena" class="form-control" />
+                        </div>
+                    </div>
+
+                    <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
+                        <button type="submit" name="submit" class="btn btn-primary btn-lg">Dodaj</button>
+                    </div>
+                    </form>
+                </div>
+                <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp" class="img-fluid" alt="Sample image">
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+    </div>
+    
+    </section>
+    <?php include 'views/footer.php';?>
+ </body>
+</html>
--- views/navbar.php
+++ views/navbar.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-dark navbar-expand bg-primary justify-content-between">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#"><i class="fa-brands fa-opencart"></i> </span><span class="d-none d-sm-inline px-1">e-commerce</span></a>
+        <a class="navbar-brand" href="index.php"><i class="fa-brands fa-opencart"></i> </span><span class="d-none d-sm-inline px-1">e-commerce</span></a>
         <ul class="navbar-nav">
             <li class="nav-item text-center">
                 <a href="index.php" class="nav-link" data-toggle="modal" data-target="#"><span class="fa fa-house"></span><span class="d-none d-sm-inline  px-1">Domov</span></a>
@@ -11,6 +11,12 @@
             <li class="nav-item text-center">
                 <a href="#" class="nav-link" data-toggle="modal" data-target="#"><span class="fa fa-address-book"></span><span class="d-none d-sm-inline  px-1">Kontakt</span></a>
             </li>
+            <?php if (isset($_SESSION["user"])){?>
+            <li class="nav-item text-center">
+                <a href="dodajArtikel.php" class="nav-link" data-toggle="modal" data-target="#"><span class="fa fa-plus"></span><span class="d-none d-sm-inline  px-1">Dodaj artikel</span></a>
+            </li>
+            <?php }else{ ?>
+            <?php } ?>
         </ul>
         <ul class="nav navbar-nav">
             <?php if (!isset($_SESSION["user"])){?>
@@ -21,9 +27,19 @@
                 <a href="login.php" class="nav-link" data-toggle="modal" data-target="#"><span class="fa fa-sign-in"></span><span class="d-none d-sm-inline px-1"> Prijava </span></a>
             </li>
             <?php }else{ ?>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Račun(<?php echo $_SESSION["user"] ?>)
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <li class="w-100">
+                            <a class="dropdown-item" href="#"><span class="fa fa-user-circle"></span><span class="d-none d-sm-inline px-1">Profil</span></a>
+                        </li>
+                    </ul>
+                </li>
                 <li class="nav-item text-center" id="logout-btn">
-                <a href="odjava.php" class="nav-link" data-toggle="modal" data-target="#"><span class="fa fa-sign-in"></span><span class="d-none d-sm-inline px-1"> Odjava </span></a>
-            </li>
+                    <a href="odjava.php" class="nav-link" data-toggle="modal" data-target="#"><span class="fa fa-sign-in"></span><span class="d-none d-sm-inline px-1"> Odjava </span></a>
+                </li>
             <?php } ?>
         </ul>
     </div>
